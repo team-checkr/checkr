49fc0bb294de7c8c3cc5630335e4199fb6a94798
Oliver Bøving <oliver@bvng.dk>
2023-04-24 11:03:09 +0200

    fix: Do short-circuiting correctly in sign analysis
    This commit also contains a small refactor to use bitsets instead of
    hashsets for storing the boolean sets in sign analysis.
    
    Fixes #42

--- checkr/src/sign.rs
+++ checkr/src/sign.rs
@@ -3,7 +3,7 @@ use std::{
     hash::Hash,
 };
 
-use itertools::{chain, Itertools};
+use itertools::{chain, Either, Itertools};
 use serde::{Deserialize, Serialize};
 
 use crate::{
@@ -105,15 +105,85 @@ impl From<Sign> for Signs {
 }
 
 impl Signs {
-    pub fn iter(self) -> impl Iterator<Item = Sign> {
+    pub fn iter(self) -> impl Iterator<Item = Sign> + Clone {
         [Sign::Positive, Sign::Zero, Sign::Negative]
             .into_iter()
             .filter(move |&s| self.contains(s.into()))
+    }
+    pub fn map(self, f: impl FnMut(Sign) -> Sign) -> Signs {
+        self.iter().map(f).collect()
     }
 }
 impl FromIterator<Sign> for Signs {
     fn from_iter<T: IntoIterator<Item = Sign>>(iter: T) -> Self {
         iter.into_iter().fold(Signs::NONE, |acc, s| acc | s.into())
+    }
+}
+bitflags::bitflags! {
+    #[derive(Serialize, Deserialize)]
+    #[serde(into = "Vec<bool>", try_from = "Vec<bool>")]
+    pub struct Bools: u8 {
+        const NONE = 0b00;
+        const FALSE = 0b01;
+        const TRUE = 0b10;
+        const ALL = Self::TRUE.bits | Self::FALSE.bits;
+    }
+}
+
+impl Default for Bools {
+    fn default() -> Self {
+        Self::NONE
+    }
+}
+
+impl std::fmt::Display for Bools {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{{{}}}", self.iter().format(", "))
+    }
+}
+
+impl From<Bools> for Vec<bool> {
+    fn from(value: Bools) -> Self {
+        value.iter().collect()
+    }
+}
+impl From<Vec<bool>> for Bools {
+    fn from(value: Vec<bool>) -> Self {
+        value.into_iter().collect()
+    }
+}
+
+#[test]
+fn bools_as_json() {
+    use std::collections::BTreeSet;
+    assert_eq!(
+        serde_json::to_string(&Bools::ALL).unwrap(),
+        serde_json::to_string(&Bools::ALL.iter().collect::<BTreeSet<_>>()).unwrap()
+    );
+}
+
+impl From<bool> for Bools {
+    fn from(value: bool) -> Self {
+        match value {
+            false => Bools::FALSE,
+            true => Bools::TRUE,
+        }
+    }
+}
+
+impl Bools {
+    pub fn iter(self) -> impl Iterator<Item = bool> + Clone {
+        [false, true]
+            .into_iter()
+            .filter(move |&s| self.contains(s.into()))
+    }
+    pub fn map(self, f: impl FnMut(bool) -> bool) -> Bools {
+        self.iter().map(f).collect()
+    }
+}
+impl FromIterator<bool> for Bools {
+    fn from_iter<T: IntoIterator<Item = bool>>(iter: T) -> Self {
+        iter.into_iter().fold(Bools::NONE, |acc, s| acc | s.into())
     }
 }
 
@@ -243,15 +313,15 @@ impl MonotoneFramework for SignAnalysis {
         match e.action() {
             Action::Assignment(Target::Variable(var), x) => prev
                 .iter()
-                .flat_map(|mem| x.semantics_sign(mem).into_iter().map(move |s| (mem, s)))
+                .flat_map(|mem| x.semantics_sign(mem).iter().map(move |s| (mem, s)))
                 .map(|(mem, s)| mem.clone().with_var(var, s))
                 .collect(),
             Action::Assignment(Target::Array(arr, idx), expr) => prev
                 .iter()
                 .flat_map(|mem| {
                     let idx_signs = idx.semantics_sign(mem);
-                    if idx_signs.contains(&Sign::Zero) || idx_signs.contains(&Sign::Positive) {
-                        let asdf: Signs = mem
+                    if idx_signs.intersects(Signs::ZERO | Signs::POSITIVE) {
+                        let array_signs: Signs = mem
                             .arrays
                             .get(arr)
                             .unwrap_or_else(|| panic!("could not get sign of array '{arr}'"))
@@ -260,18 +330,15 @@ impl MonotoneFramework for SignAnalysis {
 
                         let mut new_possible = HashSet::new();
 
-                        let signs: Signs = asdf.iter().collect();
-
-                        for s in std::iter::once(None).chain(asdf.iter().map(Some)) {
-                            let mut signs = signs;
+                        for s in std::iter::once(None).chain(array_signs.iter().map(Some)) {
+                            let mut signs = array_signs;
                             if let Some(s) = s {
                                 signs.remove(s.into());
                             }
-                            for new_sign in expr.semantics_sign(mem) {
-                                let mut signs = signs;
-                                signs.insert(new_sign.into());
+                            for new_sign in expr.semantics_sign(mem).iter() {
+                                let new_signs = signs | new_sign.into();
                                 let mut new_mem = mem.clone();
-                                new_mem.arrays.insert(arr.clone(), signs);
+                                new_mem.arrays.insert(arr.clone(), new_signs);
                                 new_possible.insert(new_mem);
                             }
                         }
@@ -285,7 +352,7 @@ impl MonotoneFramework for SignAnalysis {
             Action::Skip => prev.clone(),
             Action::Condition(b) => prev
                 .iter()
-                .filter(|mem| b.semantics_sign(mem).contains(&true))
+                .filter(|mem| b.semantics_sign(mem).contains(Bools::TRUE))
                 .cloned()
                 .collect(),
         }
@@ -303,7 +370,7 @@ impl MonotoneFramework for SignAnalysis {
 fn cartesian_flat_map<'a, L: 'a, R: 'a, T: Clone, Q>(
     l: L,
     r: R,
-    f: impl Fn(T, T) -> Q + 'a,
+    f: impl Fn(T, Option<T>) -> Q + 'a,
 ) -> impl Iterator<Item = Q> + 'a
 where
     L: IntoIterator<Item = T> + Clone,
@@ -311,34 +378,42 @@ where
     R: IntoIterator<Item = T> + Clone,
     R::IntoIter: Clone,
 {
-    l.into_iter()
-        .cartesian_product(r.into_iter())
-        .map(move |(a, b)| f(a, b))
+    if r.clone().into_iter().next().is_none() {
+        Either::Left(l.into_iter().map(move |a| f(a, None)))
+    } else {
+        Either::Right(
+            l.into_iter()
+                .cartesian_product(r.into_iter())
+                .map(move |(a, b)| f(a, Some(b))),
+        )
+    }
 }
 
 impl BExpr {
-    fn semantics_sign(&self, mem: &SignMemory) -> HashSet<bool> {
+    fn semantics_sign(&self, mem: &SignMemory) -> Bools {
         match self {
             BExpr::Bool(b) => [*b].into_iter().collect(),
-            BExpr::Rel(l, op, r) => cartesian_flat_map(
-                l.semantics_sign(mem)
-                    .iter()
-                    .flat_map(|s| s.representative()),
-                r.semantics_sign(mem)
-                    .iter()
-                    .flat_map(|s| s.representative()),
-                |l, r| op.semantic(l, r),
-            )
-            .collect(),
-            BExpr::Logic(l, op, r) => cartesian_flat_map(
-                l.semantics_sign(mem).iter().copied(),
-                r.semantics_sign(mem).iter().copied(),
-                // TODO: Follow definition with weird $S \cap {ff}$
-                |l, r| op.semantic(l, || Ok(r)),
-            )
-            .map(|res| res.expect("this could not error"))
-            .collect(),
-            BExpr::Not(b) => b.semantics_sign(mem).into_iter().map(|i| !i).collect(),
+            BExpr::Rel(l, op, r) => {
+                let l = l.semantics_sign(mem);
+                let r = r.semantics_sign(mem);
+                cartesian_flat_map(
+                    l.iter().flat_map(|s| s.representative()),
+                    r.iter().flat_map(|s| s.representative()),
+                    |l, r| Some(op.semantic(l, r?)),
+                )
+                .flatten()
+                .collect()
+            }
+            BExpr::Logic(l, op, r) => {
+                let l = l.semantics_sign(mem);
+                let r = r.semantics_sign(mem);
+                cartesian_flat_map(l.iter(), r.iter(), |l, r| {
+                    op.semantic(l, || r.ok_or(InterpreterError::NoProgression))
+                })
+                .flatten()
+                .collect()
+            }
+            BExpr::Not(b) => b.semantics_sign(mem).map(|i| !i),
             BExpr::Quantified(_, _, _) => {
                 todo!("tried to compute signs of a quantified expression")
             }
@@ -367,7 +442,7 @@ impl std::ops::Neg for Sign {
 }
 
 impl AExpr {
-    fn semantics_sign(&self, mem: &SignMemory) -> HashSet<Sign> {
+    fn semantics_sign(&self, mem: &SignMemory) -> Signs {
         match self {
             AExpr::Number(n) => [sign_of(*n)].into_iter().collect(),
             AExpr::Reference(Target::Variable(x)) => [mem
@@ -383,8 +458,9 @@ impl AExpr {
                 r.semantics_sign(mem)
                     .iter()
                     .flat_map(|x| x.representative()),
-                |l, r| op.semantic(l, r),
+                |l, r| Some(op.semantic(l, r?)),
             )
+            .flatten()
             .filter_map(|res| match res {
                 Ok(mem) => Some(mem),
                 Err(err) => match err {
@@ -403,7 +479,7 @@ impl AExpr {
             .collect(),
             AExpr::Reference(Target::Array(arr, idx)) => {
                 let idx_signs = idx.semantics_sign(mem);
-                if idx_signs.contains(&Sign::Zero) || idx_signs.contains(&Sign::Positive) {
+                if idx_signs.intersects(Signs::ZERO | Signs::POSITIVE) {
                     if let Some(arr) = mem.arrays.get(arr) {
                         arr.iter().collect()
                     } else {
@@ -413,7 +489,7 @@ impl AExpr {
                     Default::default()
                 }
             }
-            AExpr::Minus(n) => n.semantics_sign(mem).into_iter().map(|x| -x).collect(),
+            AExpr::Minus(n) => n.semantics_sign(mem).map(|x| -x),
             AExpr::Function(_) => todo!("sign of a function"),
         }
     }
