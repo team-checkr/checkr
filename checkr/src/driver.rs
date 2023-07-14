e8fdc1a05203d608bb0af059672df2aed72605b3
Martin Hansen <martin045k@gmail.com>
2023-07-15 00:31:13 +0200

    Clean up and formatting

--- checkr/src/driver.rs
+++ checkr/src/driver.rs
@@ -78,11 +78,12 @@ impl Driver {
             compile_output: Some(compile_output),
         })
     }
-    
-    //Generates a string with initial dots removed, and any initial path specifier, as well as doing the same from the back
+
+    //Generates a string with initial dot removed (if it contains this), and any initial path / or \, as well as doing the same from the back
     fn clear_format(st: String) -> String {
-        let mut process: std::collections::VecDeque<char> = std::collections::VecDeque::from(st.chars().collect::<Vec<char>>());
-        
+        let mut process: std::collections::VecDeque<char> =
+            std::collections::VecDeque::from(st.chars().collect::<Vec<char>>());
+
         //Remove initial dot if it exists and there is only 1 of them
         if !process.is_empty() && *process.front().unwrap() == '.' {
             process.pop_front();
@@ -90,39 +91,42 @@ impl Driver {
                 process.push_front('.');
             }
         }
-        
+
         //Remove / and \ from the front
-        while !process.is_empty() && (*process.front().unwrap() == '/' || *process.front().unwrap() == '\\') {
+        while !process.is_empty()
+            && (*process.front().unwrap() == '/' || *process.front().unwrap() == '\\')
+        {
             process.pop_front();
         }
-        
+
         //Remove / and \ from the back
-        while !process.is_empty() && (*process.back().unwrap() == '/' || *process.back().unwrap() == '\\') {
+        while !process.is_empty()
+            && (*process.back().unwrap() == '/' || *process.back().unwrap() == '\\')
+        {
             process.pop_back();
         }
-        
+
         let mut result: String = String::new();
-        
+
         while !process.is_empty() {
             result.push(*process.front().unwrap());
             process.pop_front();
         }
-        
+
         result
-        
     }
-    
+
     //Formats the cmd argument to have the directory pre-pended to it.
     fn format_cmd(cmd: String, dir: String) -> String {
         let format_cmd = Driver::clear_format(cmd);
         let format_dir = Driver::clear_format(dir);
         let mut result: String = String::from(""); //new cmd with directory path pre-pended to it.
-        
+
         //Checks if the path is not absolute (Single Drive letter + : on Windows)
         if !(format_dir.len() >= 2 && &format_dir[1..2] == ":") {
             result.push_str("./");
         }
-        
+
         if format_dir.len() != 0 {
             result.push_str(&format_dir);
             result.push_str("/");
@@ -130,14 +134,11 @@ impl Driver {
         result.push_str(&format_cmd);
         result
     }
-    
+
     fn new_command(&self) -> Command {
-        let new_cmd: String = Driver::format_cmd(self.run_cmd.clone(), self.dir.to_str().unwrap().to_string()); //new cmd with directory path pre-pended to it.
-        
-        eprintln!("CMD DATA: {}", &self.run_cmd);
-        eprintln!("CMD Dir: {}", self.dir.to_str().unwrap());
-        eprintln!("CMD NEW: {}", &new_cmd);
-        
+        let new_cmd: String =
+            Driver::format_cmd(self.run_cmd.clone(), self.dir.to_str().unwrap().to_string()); //new cmd with directory path pre-pended to it.
+
         let mut args = new_cmd.split(' ');
         let mut cmd = Command::new(args.next().unwrap());
         cmd.args(args);
