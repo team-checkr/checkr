351cc26aa9765476789a238faca5344d380d7380
Bajers <augustweijers@gmail.com>
2023-10-17 12:52:40 +0200

    algo for translating into z3 language

--- envs/ce-pv/src/lib.rs
+++ envs/ce-pv/src/lib.rs
@@ -37,20 +37,22 @@ pub trait Cmds {
 }
 
 pub trait Cmd {
-    fn ver_con(&self, c: BExpr) -> BExpr;
+    fn ver_con(&self, c: BExpr) -> Vec<BExpr>;
 }
 
 pub trait Ae {
     fn pretty_print(&self) -> String;
+    fn z3_ast<'ctx>(&self, ctx: &'ctx Context) -> z3int<'ctx>;
 }
 
 pub trait Be {
     fn pretty_print(&self) -> String;
+    fn z3_ast<'ctx>(&self, ctx: &'ctx Context) -> Bool<'ctx>;
 }
 
 pub trait GuardE {
-    fn ver_con_if(&self, c: BExpr) -> BExpr;
-    fn ver_con_do(&self, i: Predicate, c: BExpr) -> BExpr;
+    fn ver_con_if(&self, c: BExpr) -> Vec<BExpr>;
+    fn ver_con_do(&self, i: Predicate, c: BExpr) -> Vec<BExpr>;
 }
 
 impl Env for PvEnv {
@@ -98,63 +100,109 @@ impl Cmds for Commands {
     //Fix this at some point, so we can run unit tests
     fn ver_con(&self, cond: BExpr) -> Vec<BExpr> {
         let mut new_c_v = Vec::new();
-        let new_c = cond.clone();
+        let mut new_c = cond.clone();
         new_c_v.push(new_c);
         for n in (0..self.0.len()).rev() {
-            let new_c = self.0[n].ver_con(new_c_v.last().unwrap().clone());
-            new_c_v.push(new_c);
+            let mut new_c = self.0[n].ver_con(new_c_v.last().unwrap().clone());
+            new_c_v.append(&mut new_c);
         }
         new_c_v
     }
 }
 
 impl Cmd for Command {
-    fn ver_con(&self, c: BExpr) -> BExpr {
+    fn ver_con(&self, c: BExpr) -> Vec<BExpr> {
+        let mut new_c_v = Vec::new();
         match self {
             Command::Assignment(x, e) => {
                 if c.contains_var(x) {
-                    c.subst_var(x, e).simplify()
+                    new_c_v.push(c.subst_var(x, e).simplify())
                 } else {
-                    c.clone()
+                    new_c_v.push(c.clone())
                 }
             }
-            Command::Skip => c.clone(),
-            Command::If(guards) => guards.ver_con_if(c),
-            Command::EnrichedLoop(predicate, guards) => guards.ver_con_do(predicate.clone(), c),
+            Command::Skip => new_c_v.push(c.clone()),
+            Command::If(guards) => new_c_v.append(&mut guards.ver_con_if(c)),
+            Command::EnrichedLoop(predicate, guards) => {
+                new_c_v.append(&mut guards.ver_con_do(predicate.clone(), c))
+            }
             Command::Loop(_) => unimplemented!(),
             Command::Annotated(_, _, _) => unimplemented!(),
             Command::Continue => unimplemented!(),
             Command::Break => unimplemented!(),
-        }
+        };
+        new_c_v
     }
 }
 
 impl GuardE for Vec<Guard> {
-    fn ver_con_if(&self, c: BExpr) -> BExpr {
+    fn ver_con_if(&self, c: BExpr) -> Vec<BExpr> {
         let mut wpv = Vec::new();
+        let mut wpvl = Vec::new();
         for n in (0..self.len()).rev() {
             let cond = c.clone();
-            let l = self[n].1.ver_con(cond).last().unwrap().clone();
+            let mut left = self[n].1.ver_con(cond);
+            let l = left.last().unwrap().clone();
+            wpvl.append(&mut left);
+
             let r = self[n].0.clone();
             wpv.push(BExpr::Logic(Box::new(l), LogicOp::And, Box::new(r)));
         }
-        let mut wp = wpv.pop().unwrap();
+        let mut wp = Vec::new();
+        let mut wpif = wpv.pop().unwrap();
         for iter in wpv {
-            wp = BExpr::Logic(Box::new(wp), LogicOp::Or, Box::new(iter));
+            wpif = BExpr::Logic(Box::new(wpif.clone()), LogicOp::Or, Box::new(iter));
         }
+
+        wp.append(&mut wpvl);
+        wp.push(wpif);
         wp
     }
 
-    fn ver_con_do(&self, i: Predicate, c: BExpr) -> BExpr {
+    fn ver_con_do(&self, i: Predicate, c: BExpr) -> Vec<BExpr> {
         let mut wpv = Vec::new();
+        let mut done = Vec::new();
+        let mut qv = Vec::new();
         for n in (0..self.len()).rev() {
-            let cond = c.clone();
-            wpv.push(self[n].1.ver_con(cond).last().unwrap().clone());
+            let cond = i.clone();
+            let wp = &mut self[n].1.ver_con(cond);
+            wpv.append(&mut wp.clone());
+            qv.push(wp[0].clone());
+            done.push(self[n].0.clone());
         }
         // Do some z3 magic on this mf'er here
         // Like do it right here
         // If everything holds (Inv && Done[GC] -> Q) && (Inv && Guard) -> Weakest Pre [body](Inv), then we're good
-        i
+        let _ = env_logger::try_init();
+        let cfg = Config::new();
+        let ctx = Context::new(&cfg);
+        let inv = i.z3_ast(&ctx);
+        let solver = Solver::new(&ctx);
+
+        for n in (0..self.len()).rev() {
+            solver.assert(
+                &ast::Bool::implies(
+                    &ast::Bool::and(&ctx, &[&inv, &done[n].z3_ast(&ctx).not()]),
+                    &c.z3_ast(&ctx),
+                )
+                .not(),
+            );
+            solver.assert(
+                &ast::Bool::implies(
+                    &ast::Bool::and(&ctx, &[&inv, &self[n].0.z3_ast(&ctx)]),
+                    &qv[n].z3_ast(&ctx),
+                )
+                .not(),
+            );
+        }
+        let result = solver.check();
+        match result {
+            SatResult::Sat => panic!("Invariant does not hold"),
+            SatResult::Unsat => println!("Invariant holds"),
+            SatResult::Unknown => panic!("Z3 is confused"),
+        }
+        wpv.push(i.clone());
+        wpv
     }
 }
 
@@ -172,6 +220,26 @@ impl Ae for AExpr {
                 AOp::Pow => format!("({} ^ {})", l.pretty_print(), r.pretty_print()),
             },
             AExpr::Function(_) => todo!(),
+        }
+    }
+
+    fn z3_ast<'ctx>(&self, ctx: &'ctx Context) -> z3int<'ctx> {
+        match self {
+            AExpr::Number(n) => {
+                let mut a: i64;
+                let a = *n;
+                ast::Int::from_i64(&ctx, a)
+            }
+            AExpr::Reference(x) => ast::Int::new_const(&ctx, x.name()),
+            AExpr::Minus(m) => -m.z3_ast(&ctx),
+            AExpr::Binary(l, op, r) => match op {
+                AOp::Plus => ast::Int::add(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+                AOp::Minus => ast::Int::sub(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+                AOp::Times => ast::Int::mul(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+                AOp::Divide => ast::Int::div(&l.z3_ast(&ctx), &r.z3_ast(&ctx)),
+                AOp::Pow => todo!(),
+            },
+            AExpr::Function(f) => todo!(),
         }
     }
 }
@@ -199,19 +267,49 @@ impl Be for BExpr {
             BExpr::Quantified(_, _, _) => unimplemented!(),
         }
     }
+    fn z3_ast<'ctx>(&self, ctx: &'ctx Context) -> Bool<'ctx> {
+        match self {
+            //BExpr::Bool(b) => ast::Bool::<'a>::from_bool(&ctx, b.clone()),
+            BExpr::Bool(b) => todo!(),
+            BExpr::Rel(l, op, r) => match op {
+                RelOp::Eq => ast::Bool::from_bool(&ctx, l.z3_ast(&ctx).eq(&r.z3_ast(&ctx))),
+                RelOp::Ge => l.z3_ast(&ctx).ge(&r.z3_ast(&ctx)),
+                RelOp::Gt => l.z3_ast(&ctx).gt(&r.z3_ast(&ctx)),
+                RelOp::Le => l.z3_ast(&ctx).le(&r.z3_ast(&ctx)),
+                RelOp::Lt => l.z3_ast(&ctx).lt(&r.z3_ast(&ctx)),
+                RelOp::Ne => ast::Bool::from_bool(&ctx, !l.z3_ast(&ctx).eq(&r.z3_ast(&ctx))),
+            },
+            BExpr::Logic(l, op, r) => match op {
+                LogicOp::And => ast::Bool::and(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+                LogicOp::Implies => ast::Bool::implies(&l.z3_ast(&ctx), &r.z3_ast(&ctx)),
+                LogicOp::Land => ast::Bool::and(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+                LogicOp::Lor => ast::Bool::or(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+                LogicOp::Or => ast::Bool::or(&ctx, &[&l.z3_ast(&ctx), &r.z3_ast(&ctx)]),
+            },
+            BExpr::Not(b) => b.z3_ast(&ctx).not(),
+            BExpr::Quantified(_, _, _) => unimplemented!(),
+        }
+    }
 }
 
 #[test]
 fn pre_condition_test() {
     let pre = gcl::parse::parse_predicate("n > 5").unwrap();
-    let post = gcl::parse::parse_predicate("x>=50").unwrap();
-    let src = r#"x:=10+x;
-    x:=x*5"#;
+    let post = gcl::parse::parse_predicate("n=1").unwrap();
+    let src = r#"
+    n :=1024;
+    do {n>=10} n>1 ->
+        n:=n/2
+    od
+    "#;
     let cmds = gcl::parse::parse_commands(src).unwrap();
     let c = cmds.clone();
 
     let binding = c.ver_con(post);
     let output = binding.last().unwrap().clone();
-    println!("{}", output.simplify().pretty_print());
+    for n in (0..binding.len()).rev() {
+        println!("{}", binding[n].pretty_print());
+    }
+
     assert_eq!(output, pre);
 }
