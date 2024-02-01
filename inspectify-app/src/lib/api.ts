2f233a2dd0634052e31a2140529281f6035c7ad0
oeb25 <oliverboving@gmail.com>
2024-02-01 16:17:23 +0100

    Group types generated by tapi by common namespace prefix

--- inspectify-app/src/lib/api.ts
+++ inspectify-app/src/lib/api.ts
@@ -81,95 +81,71 @@ const sse =
       listen: (newStream) => (stream = newStream),
     };
   };
-export namespace ce_graph {
-export type GraphOutput = { dot: string }
-}
-export namespace ce_shell {export namespace io {
-export type Input = { analysis: ce_shell.Analysis, json: unknown }
-}}
-export namespace ce_shell {
-export type Envs = { "analysis": "Parse", "io": { "input": ce_parse.ParseInput, "output": ce_parse.ParseOutput } } | { "analysis": "Graph", "io": { "input": ce_graph.GraphInput, "output": ce_graph.GraphOutput } } | { "analysis": "Sign", "io": { "input": ce_sign.SignInput, "output": ce_sign.SignOutput } };
-}
-export namespace driver {export namespace job {
-export type JobId = number;
-}}
-export namespace inspectify_api {export namespace endpoints {
-export type GenerateParams = { analysis: ce_shell.Analysis }
-}}
-export namespace inspectify_api {export namespace endpoints {
-export type GclDotInput = { determinism: gcl.pg.Determinism, commands: gcl.ast.Commands }
-}}
-export namespace ce_shell {
-export type Analysis = "Parse" | "Graph" | "Sign";
-export const ANALYSIS: Analysis[] = ["Parse", "Graph", "Sign"];
-}
-export namespace ce_parse {
-export type ParseInput = { commands: gcl.ast.Commands }
-}
-export namespace ce_parse {
-export type ParseOutput = { pretty: string }
-}
-export namespace ce_graph {
-export type GraphInput = { commands: gcl.ast.Commands, determinism: gcl.pg.Determinism }
-}
-export namespace ce_sign {
-export type SignInput = { commands: gcl.ast.Commands, determinism: gcl.pg.Determinism, assignment: gcl.memory.Memory }
-}
-export namespace ce_sign {
-export type SignOutput = { initial_node: string, final_node: string, nodes: Record<string, gcl.memory.Memory[]> }
-}
-export namespace inspectify_api {export namespace endpoints {
-export type JobOutput = { output: ce_shell.io.Output, validation: ce_core.ValidationResult }
-}}
-export namespace inspectify_api {export namespace endpoints {
-export type Job = { id: driver.job.JobId, state: driver.job.JobState, kind: driver.job.JobKind, stdout: string, spans: inspectify_api.endpoints.Span[] }
-}}
-export namespace inspectify_api {export namespace endpoints {
-export type CompilationStatus = { id: driver.job.JobId, state: driver.job.JobState, error_output: (inspectify_api.endpoints.Span[] | null) }
-}}
-export namespace gcl {export namespace pg {
-export type Determinism = { "Case": "Deterministic" } | { "Case": "NonDeterministic" };
-export const DETERMINISM: Determinism[] = [{ "Case": "Deterministic" }, { "Case": "NonDeterministic" }];
-}}
-export namespace gcl {export namespace ast {
-export type Commands = string;
-}}
-export namespace gcl {export namespace memory {
-export type Memory = { variables: Record<gcl.ast.Variable, ce_sign.semantics.Sign>, arrays: Record<gcl.ast.Array, ce_sign.semantics.Signs> }
-}}
-export namespace ce_shell {export namespace io {
-export type Output = { analysis: ce_shell.Analysis, json: unknown }
-}}
 export namespace ce_core {
-export type ValidationResult = { "type": "CorrectTerminated" } | { "type": "CorrectNonTerminated", "iterations": number } | { "type": "Mismatch", "reason": string } | { "type": "TimeOut" };
+  export type ValidationResult = { "type": "CorrectTerminated" } | { "type": "CorrectNonTerminated", "iterations": number } | { "type": "Mismatch", "reason": string } | { "type": "TimeOut" };
 }
-export namespace driver {export namespace job {
-export type JobState = "Queued" | "Running" | "Succeeded" | "Canceled" | "Failed" | "Warning";
-export const JOB_STATE: JobState[] = ["Queued", "Running", "Succeeded", "Canceled", "Failed", "Warning"];
-}}
-export namespace driver {export namespace job {
-export type JobKind = { "kind": "Compilation", "data": {  } } | { "kind": "Analysis", "data": [ce_shell.Analysis, ce_shell.io.Input] };
-}}
-export namespace inspectify_api {export namespace endpoints {
-export type Span = { text: string, fg: (driver.ansi.Color | null), bg: (driver.ansi.Color | null) }
-}}
-export namespace gcl {export namespace ast {
-export type Variable = string;
-}}
-export namespace ce_sign {export namespace semantics {
-export type Sign = { "Case": "Positive" } | { "Case": "Zero" } | { "Case": "Negative" };
-export const SIGN: Sign[] = [{ "Case": "Positive" }, { "Case": "Zero" }, { "Case": "Negative" }];
-}}
-export namespace gcl {export namespace ast {
-export type Array = string;
-}}
-export namespace ce_sign {export namespace semantics {
-export type Signs = ce_sign.semantics.Sign[];
-}}
-export namespace driver {export namespace ansi {
-export type Color = "Black" | "Red" | "Green" | "Yellow" | "Blue" | "Magenta" | "Cyan" | "White" | "Default" | "BrightBlack" | "BrightRed" | "BrightGreen" | "BrightYellow" | "BrightBlue" | "BrightMagenta" | "BrightCyan" | "BrightWhite";
-export const COLOR: Color[] = ["Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White", "Default", "BrightBlack", "BrightRed", "BrightGreen", "BrightYellow", "BrightBlue", "BrightMagenta", "BrightCyan", "BrightWhite"];
-}}
+export namespace ce_graph {
+  export type GraphOutput = { dot: string }
+  export type GraphInput = { commands: gcl.ast.Commands, determinism: gcl.pg.Determinism }
+}
+export namespace ce_parse {
+  export type ParseInput = { commands: gcl.ast.Commands }
+  export type ParseOutput = { pretty: string }
+}
+export namespace ce_shell {
+  export type Envs = { "analysis": "Parse", "io": { "input": ce_parse.ParseInput, "output": ce_parse.ParseOutput } } | { "analysis": "Graph", "io": { "input": ce_graph.GraphInput, "output": ce_graph.GraphOutput } } | { "analysis": "Sign", "io": { "input": ce_sign.SignInput, "output": ce_sign.SignOutput } };
+  export type Analysis = "Parse" | "Graph" | "Sign";
+  export const ANALYSIS: Analysis[] = ["Parse", "Graph", "Sign"];
+  export namespace io {
+    export type Input = { analysis: ce_shell.Analysis, json: unknown }
+    export type Output = { analysis: ce_shell.Analysis, json: unknown }
+  }
+}
+export namespace ce_sign {
+  export type SignInput = { commands: gcl.ast.Commands, determinism: gcl.pg.Determinism, assignment: gcl.memory.Memory }
+  export type SignOutput = { initial_node: string, final_node: string, nodes: Record<string, gcl.memory.Memory[]> }
+  export namespace semantics {
+    export type Sign = { "Case": "Positive" } | { "Case": "Zero" } | { "Case": "Negative" };
+    export const SIGN: Sign[] = [{ "Case": "Positive" }, { "Case": "Zero" }, { "Case": "Negative" }];
+    export type Signs = ce_sign.semantics.Sign[];
+  }
+}
+export namespace driver {
+  export namespace ansi {
+    export type Color = "Black" | "Red" | "Green" | "Yellow" | "Blue" | "Magenta" | "Cyan" | "White" | "Default" | "BrightBlack" | "BrightRed" | "BrightGreen" | "BrightYellow" | "BrightBlue" | "BrightMagenta" | "BrightCyan" | "BrightWhite";
+    export const COLOR: Color[] = ["Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White", "Default", "BrightBlack", "BrightRed", "BrightGreen", "BrightYellow", "BrightBlue", "BrightMagenta", "BrightCyan", "BrightWhite"];
+  }
+  export namespace job {
+    export type JobId = number;
+    export type JobState = "Queued" | "Running" | "Succeeded" | "Canceled" | "Failed" | "Warning";
+    export const JOB_STATE: JobState[] = ["Queued", "Running", "Succeeded", "Canceled", "Failed", "Warning"];
+    export type JobKind = { "kind": "Compilation", "data": {  } } | { "kind": "Analysis", "data": [ce_shell.Analysis, ce_shell.io.Input] };
+  }
+}
+export namespace gcl {
+  export namespace ast {
+    export type Commands = string;
+    export type Variable = string;
+    export type Array = string;
+  }
+  export namespace memory {
+    export type Memory = { variables: Record<gcl.ast.Variable, ce_sign.semantics.Sign>, arrays: Record<gcl.ast.Array, ce_sign.semantics.Signs> }
+  }
+  export namespace pg {
+    export type Determinism = { "Case": "Deterministic" } | { "Case": "NonDeterministic" };
+    export const DETERMINISM: Determinism[] = [{ "Case": "Deterministic" }, { "Case": "NonDeterministic" }];
+  }
+}
+export namespace inspectify_api {
+  export namespace endpoints {
+    export type GenerateParams = { analysis: ce_shell.Analysis }
+    export type GclDotInput = { determinism: gcl.pg.Determinism, commands: gcl.ast.Commands }
+    export type CompilationStatus = { id: driver.job.JobId, state: driver.job.JobState, error_output: (inspectify_api.endpoints.Span[] | null) }
+    export type JobOutput = { output: ce_shell.io.Output, validation: ce_core.ValidationResult }
+    export type Job = { id: driver.job.JobId, state: driver.job.JobState, kind: driver.job.JobKind, stdout: string, spans: inspectify_api.endpoints.Span[] }
+    export type Span = { text: string, fg: (driver.ansi.Color | null), bg: (driver.ansi.Color | null) }
+  }
+}
 export const api = {
     generate: request<inspectify_api.endpoints.GenerateParams, ce_shell.io.Input>("json", "POST", "/generate", "json"),
     jobs: sse<inspectify_api.endpoints.Job[]>("/jobs", "json"),
