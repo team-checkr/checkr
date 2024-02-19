dadd294d0eaaacf0cada9b7d48c3735a1468f0dd
oeb25 <oliverboving@gmail.com>
2024-02-19 14:09:12 +0100

    Strengthen validation of calc
    Now, if no errors are present in either, then the reference compares the produced output

--- envs/ce-calc/src/lib.rs
+++ envs/ce-calc/src/lib.rs
@@ -41,26 +41,28 @@ impl Env for CalcEnv {
     fn validate(input: &Self::Input, output: &Self::Output) -> ce_core::Result<ValidationResult> {
         let reference = Self::run(input)?;
 
-        match (
-            !reference.result.is_empty(),
-            !output.result.is_empty(),
-            !reference.error.is_empty(),
-            !output.error.is_empty(),
-        ) {
-            // Both results are present
-            (true, true, _, _) => Ok(ValidationResult::CorrectTerminated),
-            // Both errors are present
-            (_, _, true, true) => Ok(ValidationResult::CorrectTerminated),
-            (_, _, _, _) => {
-                let info = format!(
-                    "Output: result={:?}, error={:?}; Reference: result={:?}, error={:?}",
-                    output.result, output.error, reference.result, reference.error,
-                );
-                Ok(ValidationResult::Mismatch {
-                    reason: format!("Did not produce same as reference. {info}"),
-                })
-            }
-        }
+        Ok(
+            match (
+                &reference.result,
+                &output.result,
+                !reference.error.is_empty(),
+                !output.error.is_empty(),
+            ) {
+                // Both errors are present
+                (_, _, true, true) => ValidationResult::CorrectTerminated,
+                // Both results are present
+                (r, o, _, _) if r == o => ValidationResult::CorrectTerminated,
+                (_, _, _, _) => {
+                    let info = format!(
+                        "Output: result={:?}, error={:?}; Reference: result={:?}, error={:?}",
+                        output.result, output.error, reference.result, reference.error,
+                    );
+                    ValidationResult::Mismatch {
+                        reason: format!("Did not produce same as reference. {info}"),
+                    }
+                }
+            },
+        )
     }
 }
 
@@ -68,14 +70,29 @@ impl Generate for CalcInput {
     type Context = ();
 
     fn gen<R: rand::Rng>(_cx: &mut Self::Context, rng: &mut R) -> Self {
-        CalcInput {
-            expression: Stringify::new(AExpr::gen(
+        let mut gen_expr = || {
+            AExpr::gen(
                 &mut GclGenContext {
                     names: Vec::new(),
                     ..GclGenContext::new(25, rng)
                 },
                 rng,
-            )),
+            )
+        };
+
+        let mut expr = gen_expr();
+        for _ in 0..10 {
+            if expr
+                .semantics(&gcl::semantics::EmptySemanticsContext)
+                .is_ok()
+            {
+                break;
+            }
+            expr = gen_expr();
+        }
+
+        CalcInput {
+            expression: Stringify::new(expr),
         }
     }
 }
