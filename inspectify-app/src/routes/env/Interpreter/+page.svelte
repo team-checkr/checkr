8c2973202563524b26f133535b6329b2e8f4547d
oeb25 <oliverboving@gmail.com>
2024-02-19 20:02:30 +0100

    Improve interpreter UI

--- inspectify-app/src/routes/env/Interpreter/+page.svelte
+++ inspectify-app/src/routes/env/Interpreter/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+  import { browser } from '$app/environment';
   import { gcl } from '$lib/api';
   import Env from '$lib/components/Env.svelte';
   import Network from '$lib/components/Network.svelte';
   import StandardInput from '$lib/components/StandardInput.svelte';
   import { useIo } from '$lib/io';
   import { toSubscript } from '$lib/fmt';
+  import ParsedInput from './ParsedInput.svelte';
 
   const io = useIo('Interpreter', {
     commands: 'skip',
@@ -12,14 +14,46 @@
     assignment: { variables: {}, arrays: {} },
     trace_length: 10,
   });
+  const { input, meta } = io;
+  $: vars = $meta ?? [];
+
+  $: if (browser) {
+    for (const v of vars) {
+      if (v.kind == 'Variable') {
+        if (typeof $input.assignment.variables[v.name] != 'number') {
+          $input.assignment.variables[v.name] = 0;
+        }
+      } else if (v.kind == 'Array') {
+        if (!Array.isArray($input.assignment.arrays[v.name])) {
+          $input.assignment.arrays[v.name] = [0];
+        }
+      }
+    }
+  }
 </script>
 
 <Env {io}>
   <svelte:fragment slot="input">
-    <StandardInput analysis="Interpreter" code="commands" {io} />
+    <StandardInput analysis="Interpreter" code="commands" {io}>
+      <h1 class="border-y p-2 pb-1 text-lg font-bold">Initial sign assignment</h1>
+      <div class="grid grid-cols-[auto_1fr] place-items-center">
+        {#each vars.slice().sort((a, b) => (a.name > b.name ? 1 : -1)) as v}
+          <div class="px-4 py-0.5 font-mono text-sm">
+            {v.name}
+          </div>
+          <div>
+            {#if v.kind == 'Array'}
+              <ParsedInput bind:value={$input.assignment.arrays[v.name]} />
+            {:else}
+              <ParsedInput bind:value={$input.assignment.variables[v.name]} />
+            {/if}
+          </div>
+        {/each}
+      </div></StandardInput
+    >
   </svelte:fragment>
-  <svelte:fragment slot="output" let:input let:output let:referenceOutput>
-    <div class="grid grid-rows-[1fr_minmax(auto,_50vh)]">
+  <svelte:fragment slot="output" let:input={cachedInput} let:output let:meta>
+    <div class="grid grid-rows-[1fr_minmax(auto,35vh)]">
       <div class="relative">
         <div class="absolute inset-0 grid overflow-auto">
           <Network dot={output.dot} />
@@ -28,19 +62,39 @@
 
       <div class="border-r border-t bg-slate-900">
         <div
-          class="grid w-full"
-          style="grid-template-columns: min-content min-content repeat(1, max-content);"
+          class="grid w-full gap-x-4 gap-y-0.5 px-4 py-2"
+          style="grid-template-columns: max-content min-content repeat({meta.length}, max-content);"
         >
-          {#each ['Action', 'Node', 'Memory'] as name}
-            <div class="border-none px-4 text-center font-mono font-bold">
+          <div />
+          <div />
+          <div
+            class="border-b text-left font-mono font-bold"
+            style="grid-column: span {meta.length}"
+          >
+            Memory
+          </div>
+          {#each ['Action', 'Node'] as name}
+            <div class="text-left font-mono font-bold">
               {name}
             </div>
           {/each}
 
-          {#each [{ action: '', node: '', memory: input.assignment }, ...output.trace] as step}
+          {#each meta as v}
+            <div class="text-center font-mono font-bold">
+              {v.name}
+            </div>
+          {/each}
+
+          {#each [{ action: '', node: output.initial_node, memory: cachedInput.assignment }, ...output.trace] as step}
             <div class="text-xs"><code>{step.action}</code></div>
             <div class="text-center">{toSubscript(step.node)}</div>
-            <div><code>{JSON.stringify(step.memory)}</code></div>
+            {#each meta as v}
+              <div class="px-1 text-right font-mono">
+                {v.kind == 'Array'
+                  ? JSON.stringify(step.memory.arrays[v.name])
+                  : step.memory.variables[v.name]}
+              </div>
+            {/each}
           {/each}
         </div>
       </div>
---
+++ inspectify-app/src/routes/env/Interpreter/ParsedInput.svelte
@@ -0,0 +1,20 @@
+<script lang="ts" generics="T">
+  export let value: T;
+  let input: string = '';
+
+  const f = (x: T) => (input = JSON.stringify(x));
+  const fInv = (x: string) => {
+    try {
+      value = JSON.parse(x);
+    } catch (err) {}
+  };
+
+  $: f(value);
+  $: fInv(input);
+</script>
+
+<input
+  type="text"
+  class="border-x-0 border-b border-t-0 bg-transparent outline-none"
+  bind:value={input}
+/>
