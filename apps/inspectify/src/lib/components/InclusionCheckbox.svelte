c69ea9cbb3fce44405de977bee9556321a6bb15c
oeb25 <oliverboving@gmail.com>
2024-04-23 14:14:40 +0200

    Sign: Add determinism and array input to inspectify

---
+++ apps/inspectify/src/lib/components/InclusionCheckbox.svelte
@@ -0,0 +1,17 @@
+<script lang="ts" generics="T">
+  export let name: string;
+  export let id: string;
+  export let value: T;
+  export let array: T[];
+
+  let input: HTMLInputElement;
+</script>
+
+<input
+  bind:this={input}
+  type="checkbox"
+  {name}
+  {id}
+  checked={array.includes(value)}
+  on:change={() => (array = input.checked ? [...array, value] : array.filter((x) => x != value))}
+/>
