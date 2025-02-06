44bf74cac3c8c460b7307c34f9f629239c47ab92
oeb25 <oliverboving@gmail.com>
2025-02-06 17:13:04 +0100

    Inline the `just build-ui` into `inspectify/build.rs`

--- crates/inspectify/build.rs
+++ crates/inspectify/build.rs
@@ -1,12 +1,46 @@
+use std::{path::PathBuf, str::FromStr};
+
 fn main() {
     // Build the frontend using `just build-ui` iff we are building in release mode.
     if std::env::var("PROFILE").unwrap() == "release" {
-        let status = std::process::Command::new("just")
-            .arg("build-ui")
+        // TODO: we should be able to do this using `just`
+        // let status = std::process::Command::new("just")
+        //     .arg("build-ui")
+        //     .stdout(std::process::Stdio::inherit())
+        //     .stderr(std::process::Stdio::inherit())
+        //     .status()
+        //     .expect("Failed to build the frontend using `just build-ui`");
+        // assert!(status.success());
+
+        // run the equivilent `cd apps/inspectify && (npm install && npm run build)`
+        let inspectify_root =
+            PathBuf::from_str(std::env::var("CARGO_MANIFEST_DIR").unwrap().as_str())
+                .unwrap()
+                .parent()
+                .unwrap()
+                .parent()
+                .unwrap()
+                .join("apps/inspectify")
+                .canonicalize()
+                .unwrap();
+
+        let status = std::process::Command::new("npm")
+            .current_dir(&inspectify_root)
+            .arg("install")
             .stdout(std::process::Stdio::inherit())
             .stderr(std::process::Stdio::inherit())
             .status()
-            .expect("Failed to build the frontend using `just build-ui`");
+            .expect("Failed to install the frontend using `npm install`");
+        assert!(status.success());
+
+        let status = std::process::Command::new("npm")
+            .current_dir(&inspectify_root)
+            .arg("run")
+            .arg("build")
+            .stdout(std::process::Stdio::inherit())
+            .stderr(std::process::Stdio::inherit())
+            .status()
+            .expect("Failed to build the frontend using `npm run build`");
         assert!(status.success());
     }
 }
