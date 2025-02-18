db280a3a31fb1ae74be70bc6f17fa766def39ecc
oeb25 <oliverboving@gmail.com>
2025-02-18 11:54:49 +0100

    Fix tailwind config for chip

--- apps/chip/tailwind.config.ts
+++ apps/chip/tailwind.config.ts
@@ -1,1 +1,11 @@
-export * from '../inspectify/tailwind.config';
+import typography from '@tailwindcss/typography';
+import forms from '@tailwindcss/forms';
+import type { Config } from 'tailwindcss';
+import resolveConfig from 'tailwindcss/resolveConfig';
+export default resolveConfig({
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [forms, typography],
+} satisfies Config);
