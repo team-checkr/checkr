d648235da801b2d17bdd2b5838618ae1fcf1d017
oeb25 <oliverboving@gmail.com>
2025-03-20 14:34:56 +0100

    Specify `node:` prefix

--- apps/inspectify/vite.config.ts
+++ apps/inspectify/vite.config.ts
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
-import path from 'path';
-import { execSync } from 'child_process';
+import path from 'node:path';
+import { execSync } from 'node:child_process';
 
 const metadata = JSON.parse(execSync('cargo metadata --format-version 1 --no-deps').toString()) as {
   packages: { name: string; version: string }[];
